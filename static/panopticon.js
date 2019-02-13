a479db44032dba9f89550b349afd06ece0ffa058
Dan Morrill <morrildl@gmail.com>
2019-02-12 23:14:55 -0800

    Fix copyright.

--- static/panopticon.js
+++ static/panopticon.js
@@ -1,4 +1,4 @@
-// Copyright © 2018 Playground Global, LLC
+// Copyright © 2019 Dan Morrill
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
